2b331eb49af28fee5038a83566707614966437b9
Matias Alexander Rømer <mati0681@edu.easj.dk>
2017-11-22 10:20:08 +0100

    ændret navn på iservice og service

--- SemesterLyd/LydService1.svc.cs
+++ SemesterLyd/LydService1.svc.cs
@@ -8,9 +8,9 @@ using System.Text;
 
 namespace SemesterLyd
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
-    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
-    public class Service1 : IService1
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "LydLydService1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select LydLydService1.svc or LydLydService1.svc.cs at the Solution Explorer and start debugging.
+    public class LydLydService1 : ILydService1
     {
         public string GetData(int value)
         {
